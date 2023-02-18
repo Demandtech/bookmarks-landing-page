17e9b0b9fa37adbf3d120c825d48c8c2ea42fed6
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-02-18 09:19:52 +0100

    extensions section implementation

---
+++ src/components/Extensions.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import styled from 'styled-components'
+import Extension from './Extension'
+import { exts } from '../utils'
+
+const Extensions = () => {
+  return (
+    <Wrapper>
+      <div className='container'>
+        <div className='header'>
+          <h2>Download the extension</h2>
+          <p>
+            We’ve got more browsers in the pipeline. Please do let us know if
+            you’ve got a favourite you’d like us to prioritize.
+          </p>
+        </div>
+        <div className='extensions-wrapper'>
+          {exts.map((ext) => {
+            return (
+              <div key={ext.id} className={ext.name.toLowerCase()}>
+                <Extension {...ext} />
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.section`
+  .container {
+    padding: 25px;
+    .header {
+      text-align: center;
+
+      h2 {
+        color: var(--veryDarkBlue);
+        font-weight: 300;
+        margin-bottom: 15px;
+        font-size: 25px;
+      }
+      p {
+        color: var(--grayishBlue);
+        line-height: 1.5;
+      }
+    }
+    .extensions-wrapper {
+      padding: 50px 0;
+    }
+  }
+  @media screen and (min-width: 835px) {
+    max-width: 1440px;
+    margin: 0 auto;
+
+    .container {
+      padding: 0 150px;
+      .header {
+        width: 50%;
+        margin: 0 auto;
+
+        p {
+          font-size: 18px;
+        }
+      }
+
+      .extensions-wrapper {
+        display: flex;
+        justify-content: space-between;
+
+        .firefox {
+          transform: translateY(30px);
+        }
+        .opera {
+          transform: translateY(60px);
+        }
+      }
+    }
+  }
+`
+
+export default Extensions
